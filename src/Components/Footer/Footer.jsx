b6488dc5618f392fe1e2e57237ea1f6f107b7215
chenaoh <darioangellopez38@gmail.com>
2024-01-19 17:30:13 -0300

    agregando el footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,28 +4,28 @@ const Footer = () => {
   return (
 <footer>
         <div className="footerStyle">
-        <div>
+        <div className="margin">
               <h4>Datos de contacto</h4>
-              <label>Email:</label>
+              <label className="margin">Email:</label>
               <a
-                className="textFooter flex justify-center items-center gap-x-2 py-1 px-2 lg:py-2 lg:px-4 text-xs text-base bg-white/5 hover:scale-110 hover:bg-white/10 transition"
+                className="textFooter"
                 href="https://mail.google.com/mail/?view=cm&to=darioangellopez38@gmail.com&su=Contacto para puesto de trabajo&body=Hola Dario, nos gustaría tener más información acerca de ti, y para ello quisiera coordinar una entrevista o abrir un canal de comunicación. Desde ya, muchas gracias."
                 target="_blank"
                 rel="noopener noreferrer">darioangellopez38@gmail.com</a>
         </div>
             <div>
-              <label>Github:</label>
+              <label className="margin">Github:</label>
                         <a
-                        className="textFooter flex justify-center items-center gap-x-2 py-1 px-2 lg:py-2 lg:px-4 text-xs text-base bg-white/5 hover:scale-110 hover:bg-white/10 transition"
+                        className="textFooter"
                         href="https://github.com/DarioLopez18"
                         target="_blank"
                         rel="noopener noreferrer"
                         >DarioLopez18</a>
             </div>
             <div>
-            <label>Linkedin</label>
+            <label className="margin">Linkedin</label>
             <a
-                    className="textFooter flex justify-center items-center gap-x-2 py-1 px-2 lg:py-2 lg:px-4 text-xs text-base bg-white/5 hover:scale-110 hover:bg-white/10 transition"
+                    className="textFooter"                   
                     href="https://www.linkedin.com/in/dario-angel-jose-lopez-2a3202234/"
                     target="_blank"
                     rel="noopener noreferrer"
