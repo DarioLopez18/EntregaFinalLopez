4b3772e9db1953af40fe075ad9fb541f50be649a
chenaoh <darioangellopez38@gmail.com>
2024-01-19 16:15:16 -0300

    arreglando el h1

--- src/Components/Icon/Icon.jsx
+++ src/Components/Icon/Icon.jsx
@@ -4,7 +4,7 @@ const Icon = () => {
   return (
       <a href="/" className="icon ">
        <img src={ecommerceIcon} className="logo" alt="Ecommerce Icon" />
-       <h1 className="h1">Ecommerce React JS</h1>
+       <h1 className="textoPrincipal">Ecommerce React JS</h1>
       </a>    
   )
 }
