c7f1c42cc110d8ad75a6b52b428830acc03d42df
chenaoh <darioangellopez38@gmail.com>
2024-01-29 14:18:30 -0300

    agregando uso de useState y useEffect

--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -9,5 +9,4 @@ const CartWidget = () => {
     </>
   )
 }
-
 export default CartWidget
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,11 +1,41 @@
 import PropTypes from "prop-types"
 import "./ItemListContainer.css"
+import { useState,useEffect } from "react";
+import Products from "../Products/Products"
 
 const ItemListContainer = (props) => {
+
+  const [products,setProducts] = useState([]);
+
+  useEffect(()=>{
+    const fetchData = async() =>{
+      try{
+        const response = await fetch("./products.json")
+        const data = await response.json()
+        setTimeout(()=>{
+          setProducts(data)
+        },2000)
+      }catch(e){
+        console.log(e)
+      }
+    }
+    fetchData()
+  },[])
+
   return (
+    <>
     <div className="base3">
         <h1 className="text">{props.greeting}</h1>
     </div>
+      <section className='productsContainer'>
+
+      {products.length == 0 ? <span className="loader"></span> : products.map(p=>{
+        return(
+          <Products key={p.id} title = {p.title} descripcion={p.descripcion} img={p.img} button={p.button}/>
+        )})}
+      </section>
+    </>
+
   )
 }
 
@@ -14,3 +44,4 @@ ItemListContainer.propTypes = {
 }
 
 export default ItemListContainer
+
