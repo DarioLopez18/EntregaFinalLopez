8d4d814d94b15dd512899b5e258d5ddd03941ddc
chenaoh <darioangellopez38@gmail.com>
2024-01-29 21:58:36 -0300

    intercambiando itemListContainer

--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -1,23 +1,9 @@
-import { useState,useEffect } from "react";
+
 import Item from "../Item/Item";
 import "./ItemList.css"
+import PropTypes from "prop-types"
 
-const ItemList = () => {
-    const [products,setProducts] = useState([]);
-    useEffect(()=>{
-        const fetchData = async() =>{
-          try{
-            const response = await fetch("./products.json")
-            const data = await response.json()
-            setTimeout(()=>{
-              setProducts(data)
-            },2000)
-          }catch(e){
-            console.log(e)
-          }
-        }
-        fetchData()
-      },[])
+const ItemList = ({products}) => {
   return (
     <section className='productsContainer'>
     {products.length == 0 ? <div className="divLoader"><h1>Cargando los productos...</h1> <span className="loader"></span></div>: products.map(p=>{
@@ -26,6 +12,10 @@ const ItemList = () => {
       )})}
     </section>
   )
+}
+
+ItemList.propTypes = {
+  products: PropTypes.array.isRequired
 }
 
 export default ItemList
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,14 +1,29 @@
 import PropTypes from "prop-types"
 import "./ItemListContainer.css"
 import ItemList from "../ItemList/ItemList"
+import { useState,useEffect } from "react";
 
 const ItemListContainer = ({greeting}) => {
-
+  const [products,setProducts] = useState([]);
+  useEffect(()=>{
+      const fetchData = async() =>{
+        try{
+          const response = await fetch("./products.json")
+          const data = await response.json()
+          setTimeout(()=>{
+            setProducts(data)
+          },2000)
+        }catch(e){
+          console.log(e)
+        }
+      }
+      fetchData()
+    },[])
   return (
     <>
     <div className="base3">
         <h1 className="text">{greeting}</h1>
-        <ItemList/>
+        <ItemList products = {products}/>
     </div>
     </>
 
