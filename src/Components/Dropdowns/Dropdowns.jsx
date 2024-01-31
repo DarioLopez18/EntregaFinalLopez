690809e57092fce8afd220c47a259d760303acf8
chenaoh <darioangellopez38@gmail.com>
2024-01-31 12:44:49 -0300

    mejorando el dropdown y agregando las categorias

--- src/Components/Dropdowns/Dropdowns.jsx
+++ src/Components/Dropdowns/Dropdowns.jsx
@@ -1,13 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Dropdown, DropdownItem,DropdownMenu, DropdownToggle } from "reactstrap";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Dropdowns.css"
+import Loader from "../Loader/Loader";
 
 const Dropdowns = () => {
   const [dropdown,setDropdown] = useState(false);
+  const [categories,setCategories] = useState([]);
   const abrirCerrrarDropdown = () =>{
     setDropdown(!dropdown)
   }
+  useEffect(()=>{
+    const fetchData = async() =>{
+      try{
+        const response = await fetch("./categories.json")
+        const data = await response.json()
+        setCategories(data)
+      }catch(e){
+        console.log(e)
+      }
+    }
+    fetchData()
+  },[])
   return (
     <div>
        <Dropdown className="text" isOpen={dropdown} toggle={abrirCerrrarDropdown}>
@@ -18,27 +32,14 @@ const Dropdowns = () => {
             <DropdownItem disabled>
                 Seleccione una
             </DropdownItem>
-            <DropdownItem className="item" >
-                NOTEBOOKS
-            </DropdownItem>
-            <DropdownItem className="item" >
-                MOTHERBOARDS
-            </DropdownItem >
-            <DropdownItem className="item" >
-                FUENTES
-            </DropdownItem>
-            <DropdownItem className="item" >
-                MOUSE
-            </DropdownItem>
-            <DropdownItem className="item" >
-                MONITORES
-            </DropdownItem>
-            <DropdownItem className="item" >
-                GPU
-            </DropdownItem>
-            <DropdownItem className="item" >
-                PROCESADORES
-            </DropdownItem>
+            {categories.length == 0 ? <Loader text="Cargando categorias..."/> : categories.map(categoria=>{
+              return(
+                <DropdownItem key={categoria.id} className="item">
+                {categoria.name}
+                </DropdownItem>
+              )
+            })}
+
          </DropdownMenu>
        </Dropdown>
     </div>
