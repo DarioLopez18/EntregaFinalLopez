2618f2bcfa12e7332f4bf71e450edf544a4a3813
chenaoh <darioangellopez38@gmail.com>
2024-02-05 19:33:54 -0300

    agregando useFetch

---
+++ src/Hooks/useFetch/useFetch.jsx
@@ -0,0 +1,14 @@
+import { useEffect,useState } from "react"
+
+export const useFetch = (url) =>{
+    const [data,setData] = useState(null)
+
+    useEffect(()=>{
+      fetch(url)
+        .then(response =>{ response.json()})
+        .then(data => setData(data))  
+        .catch(error => console.log(error))
+    })
+
+    return data
+}
