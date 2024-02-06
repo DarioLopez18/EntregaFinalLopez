32686506da839a3a6e55253d810e8b8ef59fdf3c
chenaoh <darioangellopez38@gmail.com>
2024-02-05 21:35:56 -0300

    comenzando con la navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,34 @@ import Footer from './Components/Footer/Footer'
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
 import Navbar from './Components/Navbar/Navbar'
 import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <Navbar/>
-      <ItemListContainer greeting='¡Bienvenidos a mi Ecommerce hecha con React JS!'/>
-      <Footer/>
-      <ItemDetailContainer id={1}/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element = {
+              <ItemListContainer greeting='¡Bienvenidos a mi Ecommerce hecha con React JS!'/>
+          }/>
+          <Route path='/category/:id' element = {
+              <></>
+          }/>
+          <Route path='/item/:id' element = {
+              <ItemDetailContainer id={1}/>
+          }/>
+          <Route path='/cart' element = {
+              <></>
+          }/>
+          <Route path='/checkout' element = {
+              <></>
+          }/>
+        </Routes>
+        <Footer/>
+
+      </BrowserRouter>
     </>
   )
 }
