836960cce1f879a9252cfdfe6603a7f47500441c
chenaoh <darioangellopez38@gmail.com>
2024-02-19 13:39:24 -0300

    agregando env

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAw-t_HHGsZrlr__XUzolfK8XhjE76K4CA",
+  authDomain: "coderhouse-ecommerce-d771b.firebaseapp.com",
+  projectId: "coderhouse-ecommerce-d771b",
+  storageBucket: "coderhouse-ecommerce-d771b.appspot.com",
+  messagingSenderId: "939568691128",
+  appId: "1:939568691128:web:40934607c53b6a6321014d",
+  measurementId: "G-8MKP7MVCWN"
+};
+
+initializeApp(firebaseConfig);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import env from 'vite-plugin-env-compatible'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    reactRefresh(),
+    env(".env")
+  ],
+});
+
